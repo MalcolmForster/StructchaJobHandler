e4511819eb1e67d799054212e8f47b35dd0d25d1
Malcolm <forster.malcolm@gmail.com>
2022-11-11 22:19:27 +1300

    Messing around with C++, included soci

--- StructchaJobHandler/Handler.cpp
+++ StructchaJobHandler/Handler.cpp
@@ -1,2 +1,31 @@
-import <iostream.h>
+#include <soci/soci.h>
+#include <iostream>
+using namespace std;
 
+/*
+    This main file starts all the other services when required which are:
+    - Check if request for job status is called
+    - Checking if there is a job to run/pause/stop/finished
+    - Passing data too and retrieving data from the database
+
+*/ 
+
+int main() {
+    cout << "Starting StructchaHandler\n";
+    cout << "...\n";
+    //initilisation phase
+
+    cout << "StructchaHandler Started and running with no errors\n";
+    cout << "Looking for tasks\n";
+
+    int i = 0;
+    while(i < 1'000'000'000) {
+        if(i % 100'000'000 == 0) {
+            cout << i << "\n";
+        }        
+        i++;
+    }    
+
+    cout << "\nStructchaHandler has exited\n";
+    return 0;
+}
