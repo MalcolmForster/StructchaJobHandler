4338a3541b43501cd22d8e1591faac973a613ae2
Malcolm Forster <forster.malcolm@gmail.com>
2023-01-16 23:01:47 +1300

    Added Hex convert and begun AnalysisFile Class

--- StructchaJobChecker/Checker.cpp
+++ StructchaJobChecker/Checker.cpp
@@ -2,7 +2,8 @@
 #include <fstream>
 #include <bitset>
 #include <string>
-using "../lib/structcha_files/analysis_file.hpp" as analysisFile;
+#include <vector>
+#include "../lib/structcha_files/analysis_file.hpp"
 
 using namespace std;
 
@@ -16,24 +17,30 @@ int main() {
     //First section converts file from binary to readable text so information can be extracted.
     binaryFile.open("../TestFiles/TestAnalysis.scan",ios::in); // open the file which was written to before (ie the one in binary)
     tempFile.open("CheckingFile.txt",ios::out);
-
-    analysisFile
-        
+           
     while (getline(binaryFile, tp)) { // not needed as all binary is on one line I believe    
         for(size_t i = 0; i < (tp.size()/8); i++) {
             string bit_string = (tp.substr(i*8, 8));
             std::bitset<8> b(bit_string);
             unsigned char c = ( b.to_ulong());
             tempFile << c;
-        }            
+        }
     }
 
     binaryFile.close();
     tempFile.close();
 
+    tempFile.open("CheckingFile.txt", ios::in);
+    vector<string> fileLines;
 
-
-
+    while (getline(tempFile,tp)){ //retrieves all value information from the file and saves into array
+        if(tp.substr(0,2) != "//" && tp.size() != 0) {
+            fileLines.push_back(tp);
+        }
+    }    
+    
+    StructchaAnalysisFile saf = setAnalysisFile(fileLines); // sets the Analysis object according to the analysis type.
+    tempFile.close();
 
     return 0;
 }
--- lib/structcha_files/analysis_file.hpp
+++ lib/structcha_files/analysis_file.hpp
@@ -1,19 +1,56 @@
 #include <fstream>
+#include <iostream>
+#include <variant>
+#include <string.h>
+
+using namespace std;
 
 class StructchaAnalysisFile {
-    public string JobName {get;set;};
-    public string 
+    public:
+        string JobName;
+        string Company;
+        string User;
+        string Type;
+        int number;
+        int array1[5];
+        int array2[5];
+        StructchaAnalysisFile(std::vector<string> analysisFile) {
+            if(analysisFile[0].compare("[[StructchaAnalysis<<HEADER]]]")) {
+                string s = "";
+                int i = 1;
+                int dataLineCount = analysisFile.size();
+                while (analysisFile[i].compare("[[StructchaAnalysis>>HEADER]]") && i < dataLineCount) {
+                    s = analysisFile[i];
+                    cout << "HL: "<< s << endl;
+                    i++;
+                }
+                i++;
+                if(analysisFile[i].compare("[[StructchaAnalysis<<DATA")) {
+                    i++;
+                    while (analysisFile[i].compare("[[StructchaAnalysis>>DATA]]") && i < dataLineCount) {
+                        s = analysisFile[i];
+                        cout << "DL: "<< s << endl;
+                        i++;
+                    }
+                }
 
-    public StructchaAnalysisFile(fstream analysisFile) {
+            }
+        }
+};
 
-    }
-}
+// class TestAnalysisFile: public StructchaAnalysisFile {
+//     public:
+//         int number;
+//         int array1[5];
+//         int array2[5];    
+// };
 
-private class TestAnalysisFile {
-    public int number;
-    public int[] array1;
-    public int[] array2;
+StructchaAnalysisFile setAnalysisFile(std::vector<string> analysisFile) {    
+    // for(size_t i=0; i < analysisFile.size(); i++) {
+    //     cout << analysisFile[i] << endl;
+    // }
 
-    
-}
+    StructchaAnalysisFile saf = StructchaAnalysisFile(analysisFile);
+    return saf;
+};
 
